51fb541ec4b01431746edadc15c5b2cbcc25bb9b
lilbobb <ukurowo@gmail.com>
2023-03-21 14:13:36 +0100

    On branch main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
 
 const Navbar = () => {
   return (
     <div>
       <nav className="nav">
         <div className="brand">BobTech</div>
-        <ul>
+        <div className="navbar-bars">
+          <a href="#" className="navbar-bar"></a>
+          <a href="#" className="navbar-bar"></a>
+          <a href="#" className="navbar-bar"></a>
+        </div>
+        <ul className="navbar-links">
+          <li>
+            <i className="fa-solid fa-bars"></i>
+          </li>
           <li>
             <Link to="/" className="pages">
               Home
